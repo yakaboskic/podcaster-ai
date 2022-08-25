e4ac36f6e45746369d29423f9c1f9991aa071698
Chase Yakaboski <chase.a.yakaboski@dartmouth.edu>
2022-08-25 12:21:26 -0400

    Initial implementation of midlevel DL model, not replicating results.

--- podcaster_ai/midlevel_features.py
+++ podcaster_ai/midlevel_features.py
@@ -3,8 +3,10 @@ import numpy as np
 import librosa
 import tensorflow as tf
 import math
+import compress_pickle
+import tqdm
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers, activations
 from collections import defaultdict
 
 
@@ -46,22 +48,224 @@ def random_hopify(in_signal, sr, window_size_seconds, num_hops):
 def get_melspectrograms(hops, sr, log=True):
     mels = []
     for row in hops:
-        mel = librosa.melspectrogram(y=row, sr=sr)
+        mel = librosa.feature.melspectrogram(y=row, sr=sr, hop_length=int(sr/30.25), n_mels=149)
         if log:
-            mel = librosa.core.power_to_db(mels, amin=1e-7)
-        mels.append(mels)
-    return np.array(rows)
+            mel = librosa.core.power_to_db(mel)
+        mels.append(mel)
+    return np.array(mels)
 
-def transform_sample(audio_path, sampling_rate=22050, hop_size_seconds=None, window_size_seconds=10, num_random_hops=None):
-    # Load audio using librosa and resample to sampling rate and convert to mono
-    in_signal, in_sr = librosa.load(audio_path, sr=sampling_rate, mono=True)
+def transform_sample(in_signal, in_sr, hop_size_seconds=None, window_size_seconds=10, num_random_hops=None):
     # Hopify to get audio data in the appropriate length
-    if random_hops:
+    if num_random_hops:
         hops = random_hopify(in_signal, in_sr, window_size_seconds, num_random_hops)
     else:
         hops = hopify(in_signal, in_sr, hop_size_seconds, window_size_seconds)
     # Get a melspectrogram for each hop
     mels = get_melspectrograms(hops, in_sr)
+    return mels
 
-def construct_dataset(audio_paths, sampling_rate, hop_size_seconds=None, window_size_seconds=None, num_random_hops=None, batch_size=128):
+def construct_datarow(x, y, sr, hop_size_seconds, window_size_seconds, num_random_hops):
+    # Get hops and melspectrograms
+    x_mels = transform_sample(x, sr, hop_size_seconds, window_size_seconds, num_random_hops)
+    # Duplicate the y's for each hop
+    y_mels = np.full((x_mels.shape[0], y.shape[0]), y)
+    return x_mels, y_mels
 
+def transform_generator(dataset_path, sr=22050, window_size_seconds=10, y_scale=0.1):
+    with open(dataset_path, 'rb') as data_file:
+        X, Y, _ = compress_pickle.load(data_file)
+    for x, y in zip(X, Y):
+        x_mel = transform_sample(x, sr, window_size_seconds=window_size_seconds, num_random_hops=1)
+        x_mel = np.squeeze(x_mel, 0)#.T
+        x_mel = np.expand_dims(x_mel, -1)
+        yield x_mel.astype('float32'), y.astype('float32') * y_scale
+
+def construct_dataset(dataset_path, sr, hop_size_seconds=None, window_size_seconds=None, num_random_hops=None, batch_size=128):
+    # Load pre-built dataset
+    with open(dataset_path, 'rb') as data_file:
+        X, Y, Y_cols = compress_pickle.load(data_file)
+    # Transform each sample
+    X_mels, Y_mels = [], [] 
+    for x, y in tqdm.tqdm(zip(X, Y), desc='Transforming rows', total=len(X)):
+        x_mels, y_mels = construct_datarow(x, y, sr, hop_size_seconds, window_size_seconds, num_random_hops)
+        X_mels.append(x_mels)
+        Y_mels.append(y_mels)
+    # Concatenate arrays
+    X_new = np.vstack(X_mels)
+    Y_new = np.vstack(Y_mels)
+    return X_new, Y_new, Y_cols
+
+def train_jku_emotions_model(
+        dataset_path,
+        learning_rate=0.0005,
+        epsilon=1.0,
+        batch_size=8,
+        epochs=50,
+        ):
+    print('Loading dataset and dataset shapes for model construction.')
+    # Load pre-built dataset to get column names
+    with open(dataset_path, 'rb') as data_file:
+        X, _, y_cols = compress_pickle.load(data_file)
+    # Build generator to get necessary shapes
+    for x, y in transform_generator(dataset_path):
+        break
+    print('Constructing model.')
+    # Build model
+    model = construct_jku_emotions_model(x.shape, y_cols)
+    # Compile model
+    model = compile_jku_emotions_model(model, keras.optimizers.Adam, y_cols, learning_rate, epsilon)
+    # Construct tf dataset
+    dataset = tf.data.Dataset.from_generator(
+            transform_generator,
+            args=[dataset_path], 
+            output_signature=(
+                tf.TensorSpec(shape=x.shape, dtype=tf.float32),
+                tf.TensorSpec(shape=y.shape, dtype=tf.float32),
+                )
+            ).shuffle(50).batch(batch_size)
+    # Fit
+    history = model.fit(
+        x=dataset,
+        epochs=epochs,
+        )
+    return model, history
+
+def compile_jku_emotions_model(model, optimizer, output_columns, learning_rate=0.0005, epsilon=1.0):
+    # Build list of mean square error losses
+    losses = []
+    for _ in output_columns:
+        losses.append(keras.losses.MeanSquaredError())
+    # Compile
+    model.compile(
+            optimizer=optimizer(
+                learning_rate=learning_rate,
+                epsilon=epsilon,
+                ),
+            loss=losses,
+            )
+    return model
+
+def construct_jku_emotions_model(input_shape, output_columns):
+    # Construct base model
+    inputs, x = construct_jku_base_model(input_shape)
+    # Define outputs
+    outputs = []
+    for out_name in output_columns:
+        outputs.append(
+                layers.Dense(1, name=out_name)(x)
+                )
+    model = keras.Model(
+            inputs=inputs,
+            outputs=outputs,
+            )
+    return model
+
+def construct_jku_base_model(input_shape):
+    # Input Layer
+    inputs = keras.Input(shape=input_shape, name="mel_inputs")
+    # First Convolution
+    x = layers.Conv2D(
+            filters=64,
+            kernel_size=5,
+            strides=2,
+            padding='same',
+            #activation='relu',
+            )(inputs)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # Second Convolution
+    x = layers.Conv2D(
+            filters=64,
+            kernel_size=3,
+            strides=1,
+            padding='same',
+            #activation='relu',
+            )(x)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # First Max Pooling
+    x = layers.MaxPool2D(pool_size=2)(x)
+    # Dropout
+    x = layers.Dropout(0.3)(x)
+    # Third Convolution
+    x = layers.Conv2D(
+            filters=128,
+            kernel_size=3,
+            strides=1,
+            padding='same',
+            #activation='relu',
+            )(x)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # Fourth Convolution
+    x = layers.Conv2D(
+            filters=128,
+            kernel_size=3,
+            strides=1,
+            padding='same',
+            #activation='relu',
+            )(x)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # Second Max Pooling
+    x = layers.MaxPool2D(pool_size=2)(x)
+    # Dropout
+    x = layers.Dropout(0.3)(x)
+    # Fifth Convolution
+    x = layers.Conv2D(
+            filters=256,
+            kernel_size=3,
+            strides=1,
+            padding='same',
+            #activation='relu',
+            )(x)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # Sixth Convolution
+    x = layers.Conv2D(
+            filters=256,
+            kernel_size=3,
+            strides=1,
+            padding='same',
+            #activation='relu',
+            )(x)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # Seventh Convolution
+    x = layers.Conv2D(
+            filters=384,
+            kernel_size=3,
+            strides=1,
+            padding='same',
+            #activation='relu',
+            )(x)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # Eighth Convolution
+    x = layers.Conv2D(
+            filters=512,
+            kernel_size=3,
+            strides=1,
+            padding='same',
+            #activation='relu',
+            )(x)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # Nineth Convolution
+    x = layers.Conv2D(
+            filters=256,
+            kernel_size=3,
+            strides=1,
+            padding='valid',
+            #activation='relu',
+            )(x)
+    x = layers.BatchNormalization()(x)
+    x = activations.relu(x)
+    # Adapative average pooling
+    x = layers.AveragePooling2D(
+            pool_size=1,
+            strides=1,
+            )(x)
+    # Flatten
+    x = layers.Flatten()(x)
+    return inputs, x
--- scripts.py
+++ scripts.py
@@ -1,5 +1,7 @@
 import subprocess
 import json
+import sys
+import os
 
 from podcaster_ai.util.spotify_data import get_client, get_box_fileids
 
@@ -71,3 +73,32 @@ def fileids():
     fileids_map = get_box_fileids(client, dir_blacklist=DIR_BLACKLIST)
     with open('spotify-fileids.json', 'w') as f_:
         json.dump(fileids_map, f_)
+
+def collect_audio_data():
+    import compress_pickle
+    import numpy as np
+    import concurrent.futures as cf
+    import tqdm
+    import librosa
+    import audioread.ffdec
+    from operator import itemgetter
+
+    dir_path = sys.argv[-2]
+    out_path = sys.argv[-1]
+
+    files = os.listdir(dir_path)
+
+    def process_file(path):
+        name, _ = os.path.splitext(path)
+        aro = audioread.ffdec.FFmpegAudioFile(os.path.join(dir_path, path))
+        x, _ = librosa.load(aro)
+        return (int(name), x)
+
+    with cf.ThreadPoolExecutor() as executor:
+        results = list(tqdm.tqdm(executor.map(process_file, files), total=len(files)))
+
+    results = sorted(results, key=itemgetter(0))
+    np_res = [arr for _, arr in results]
+    with open(out_path, 'wb') as data_file:
+        compress_pickle.dump(np_res, data_file)
+    print('Complete.')
